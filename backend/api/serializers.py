00ae69ef0e3e16d2267b8c142cc73653e8937f0b
Selen Karakaya <122388950+selenkarakaya@users.noreply.github.com>
2025-01-07 13:33:54 +0000

    update

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from .models import Appt
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -12,3 +13,20 @@ class UserSerializer(serializers.ModelSerializer):
         user = User.objects.create_user(**validated_data)
         return user
         
+
+class ApptSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Appt
+        fields=["id", "service", "message","created_at", "author"]
+        extra_kwargs ={" author":{"read_only":True}} 
+
+
+'''
+
+extra_kwargs = {"author": {"read_only": True}}:
+
+This is an additional configuration for the serializer fields. It indicates that the author field should be read-only, meaning it cannot be modified or written to when creating or updating an Appt instance via the API.
+read_only=True: This ensures that the author field will be included in the serialized data (for example, in GET requests), but it won't be accepted in POST or PUT requests to create or update Appt instances.
+
+
+'''        
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import generics
-from .serializers import UserSerializer
+from .serializers import UserSerializer, ApptSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
+
 # Create your views here.
 
 class CreateUserView(generics.CreateAPIView): #Automatically provides support for HTTP POST requests to create a new resource.
