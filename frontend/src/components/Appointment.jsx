da31ee02fd050070f22c6585b9a523f2a0cddef9
Selen Karakaya <122388950+selenkarakaya@users.noreply.github.com>
2025-01-17 11:31:52 +0000

    update

--- frontend/src/components/Appointment.jsx
+++ frontend/src/components/Appointment.jsx
@@ -2,22 +2,55 @@ import { Link } from "react-router-dom";
 import { ImCancelCircle } from "react-icons/im";
 import { MdEditNote } from "react-icons/md";
 function Appointment({ appointment, onDelete }) {
+  //date-time format for style
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    return date.toLocaleString("en-GB", {
+      weekday: "long", //A day name like Monday.
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+      hour: "numeric",
+      minute: "numeric",
+      hour12: false, //24-hour format.
+    });
+  };
+  // Dynamically set the status based on the appointment date
+  const getStatus = (appointmentDate) => {
+    const currentDate = new Date();
+    const appointmentDateTime = new Date(appointmentDate);
+    if (appointmentDateTime < currentDate) {
+      return "closed";
+    } else {
+      return "open";
+    }
+  };
   return (
     <tr className="text-center">
       <td>{appointment.service}</td>
       <td>{appointment.message}</td>
-      <td>date{appointment.date}</td>
-      <td>open{appointment.status}</td>
-      {/* <div className={`status status-${appointment.status}`}>
-        {appointment.status}
-      </div>  */}
+      <td>{formatDate(appointment.appointment_date)}</td>
+      <td>
+        <span className={`status ${getStatus(appointment.appointment_date)}`}>
+          {getStatus(appointment.appointment_date)}
+        </span>
+      </td>
       <td>
         <div className="flex justify-center items-center space-x-4">
           {/* Cancel Button with Tooltip */}
           <div className="relative group flex items-center">
-            <button onClick={() => onDelete(appointment.id)}>
+            <button
+              onClick={() => onDelete(appointment.id)}
+              disabled={getStatus(appointment.appointment_date) === "closed"} //The button is disabled.
+            >
               <ImCancelCircle
-                style={{ color: "#BE3144", fontSize: "1.5rem" }}
+                style={{
+                  color:
+                    getStatus(appointment.appointment_date) === "closed"
+                      ? "#d3d3d3"
+                      : "#BE3144",
+                  fontSize: "1.5rem",
+                }}
               />
             </button>
             {/* Tooltip for Cancel */}
@@ -28,10 +61,24 @@ function Appointment({ appointment, onDelete }) {
 
           {/* Edit Button with Tooltip */}
           <div className="relative group flex items-center">
-            <Link to={`/editAppointment-form/${appointment.id}`}>
+            <Link
+              to={`/editAppointment-form/${appointment.id}`}
+              style={{
+                pointerEvents:
+                  getStatus(appointment.appointment_date) === "closed"
+                    ? "none"
+                    : "auto",
+              }} //The link is disabled.
+            >
               <MdEditNote
                 className="text-darkBlue"
-                style={{ fontSize: "2rem" }}
+                style={{
+                  fontSize: "2rem",
+                  color:
+                    getStatus(appointment.appointment_date) === "closed"
+                      ? "#d3d3d3"
+                      : "",
+                }}
               />
             </Link>
             {/* Tooltip for Edit */}
