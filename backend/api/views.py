2b41f3d2d0cd14f6a7f5d6a1f15fb59be4e0e1e8
Selen Karakaya <122388950+selenkarakaya@users.noreply.github.com>
2025-01-20 16:56:39 +0000

    update

--- backend/api/views.py
+++ backend/api/views.py
@@ -208,19 +208,6 @@ class ApptListView(generics.ListAPIView):
         return Appt.objects.filter(author=user)    
 
     
-# class FeedbackView(APIView):
-#     permission_classes = [IsAuthenticated]
-#     parser_classes = (MultiPartParser, FormParser)
-
-#     def post(self, request, *args, **kwargs):
-#         data = request.data.copy()  # Gelen isteği düzenlemek için bir kopya oluştur
-#         data['user'] = request.user # Oturum açmış kullanıcının ID'sini ekle
-#         serializer = FeedbackSerializer(data=data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=201)
-#         return Response(serializer.errors, status=400)  
-    
 class FeedbackView(APIView):
     permission_classes = [IsAuthenticated]
     parser_classes = (MultiPartParser, FormParser)
