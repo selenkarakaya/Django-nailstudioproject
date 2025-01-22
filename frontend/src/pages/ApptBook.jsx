d3f89dc62cefbca7d9fd88ea9f53234565254ba8
Selen Karakaya <122388950+selenkarakaya@users.noreply.github.com>
2025-01-22 12:54:40 +0000

    update

--- frontend/src/pages/ApptBook.jsx
+++ frontend/src/pages/ApptBook.jsx
@@ -1,6 +1,10 @@
+import { useContext } from "react";
+import { Link } from "react-router-dom";
 import ApptForm from "../components/ApptForm";
+import UserContext from "../context/UserContext";
 
 function ApptBook() {
+  const { user } = useContext(UserContext);
   return (
     <div>
       <section className="flex flex-col items-center">
@@ -8,7 +12,13 @@ function ApptBook() {
         <p>Please choose from an option below</p>
         <h1 className="mb-4">Fresh Nail • Fresh You</h1>
       </section>
-      <ApptForm />
+      {user ? (
+        <ApptForm />
+      ) : (
+        <button className="bg-darkBlue border-2 border-darkBlue px-4 py-2 rounded-lg text-center hover:bg-transparent hover:text-darkBlue transition duration-1000 delay-150 mr-1">
+          <Link to="/login">Log in</Link>
+        </button>
+      )}
     </div>
   );
 }
