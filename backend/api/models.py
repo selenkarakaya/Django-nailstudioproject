98ff6990c3dd46335c2f2fbcde75c2cf37023151
Selen Karakaya <122388950+selenkarakaya@users.noreply.github.com>
2025-01-22 14:17:53 +0000

    update

--- backend/api/models.py
+++ backend/api/models.py
@@ -16,8 +16,6 @@ class Appt(models.Model):
         return self.service, self.message, self.author, self.appointment_date, self.status
     
 
-from django.db import models
-from django.contrib.auth.models import User
 
 class Feedback(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)  #user info
