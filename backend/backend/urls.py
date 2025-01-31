aa7cb753add6dc1837b4888dea66d5cb1b261cee
Selen Karakaya <122388950+selenkarakaya@users.noreply.github.com>
2025-01-31 19:47:35 +0000

    update

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -5,8 +5,6 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.conf import settings
 from django.conf.urls.static import static
 
-
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/user/register/", CreateUserView.as_view(), name="register"),
