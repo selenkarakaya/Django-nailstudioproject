683030a1329c1be24bd6c03282c0442e3b78978f
Selen Karakaya <122388950+selenkarakaya@users.noreply.github.com>
2025-03-05 13:46:53 +0000

    update

--- Procfile
+++
@@ -1,3 +0,0 @@
-web: gunicorn backend.wsgi:application --bind 0.0.0.0:$PORT
-
-
--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create an Axios instance with base URL and credentials enabled
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000/api/",
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true, // Ensures cookies are sent with requests
 });
 
